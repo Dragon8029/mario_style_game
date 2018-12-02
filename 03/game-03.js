2f400a1729404493113264048eab292448af4a3b
Cam Utz <Dragon8029@gmail.com>
2018-12-01 18:21:38 -0700

    more bug fixes

--- 03/game-03.js
+++ 03/game-03.js
@@ -61,7 +61,7 @@ Game.World.prototype = {
         if (object.x < 0) { object.x = 0; object.velocity_x = 0; }
         else if (object.x + object.width > this.width) { object.x = this.width - object.width; object.velocity_x = 0; }
         if (object.y < 0) { object.y = 0; object.velocity_y = 0; }
-        else if (object.y + object.height > this.height) { object.jumping = false; object.y = this.height - object.heigth; object.velocity_y = 0; }
+        else if (object.y + object.height > this.height) { object.jumping = false; object.y = this.height - object.height; object.velocity_y = 0; }
 
     },
 
