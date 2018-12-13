a2848283276221ac5f833cf960d38ebe4a83fc14
Cam Utz <Dragon8029@gmail.com>
2018-12-13 09:54:04 -0700

    more bug fixes to main-05.js

--- 05/main-05.js
+++ 05/main-05.js
@@ -84,7 +84,7 @@ window.addEventListener("load", function(event) {
     var controller = new Controller();
     var display = new Display(document.querySelector("canvas"));
     var game = new Game();
-    var engine = new engine(1000/30, render, update);
+    var engine = new Engine(1000/30, render, update);
 
       ////////////////////
      /// INITIALIZE /////
